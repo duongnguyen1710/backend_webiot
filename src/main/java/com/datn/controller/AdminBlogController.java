3eb8c9e5ece0cbf910b50cd589bf3c0c6852957f
duongnguyen17 <nguyenduong171002@gmail.com>
2025-02-27 17:31:46 +0700

    fix blog

--- src/main/java/com/datn/controller/AdminBlogController.java
+++ src/main/java/com/datn/controller/AdminBlogController.java
@@ -1,24 +1,19 @@
 package com.datn.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import com.datn.service.CloudinaryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.datn.entity.Blog;
 import com.datn.response.BlogResponse;
 import com.datn.service.BlogService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/admin/blog")
@@ -26,7 +21,33 @@ import com.datn.service.BlogService;
 public class AdminBlogController {
 	@Autowired
 	private BlogService blogService;
-	
+
+    @Autowired
+    private CloudinaryService cloudinaryService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @PostMapping(consumes = {"multipart/form-data"})
+//    public ResponseEntity<Blog> createBlog(
+//            @RequestPart("request") String requestJson,
+//            @RequestPart(value = "images", required = false) List<MultipartFile> images) throws IOException {
+//
+//        // Chuyển JSON từ chuỗi thành object Blog
+//        Blog blog = objectMapper.readValue(requestJson, Blog.class);
+//
+//        // Nếu có ảnh tải lên, upload và cập nhật danh sách URL
+//        if (images != null && !images.isEmpty()) {
+//            List<String> imageUrls = cloudinaryService.uploadImages(images);
+//            blog.setImages(imageUrls);
+//        }
+//
+//        // Lưu blog vào database
+//        Blog createdBlog = blogService.createBlog(blog);
+//
+//        return ResponseEntity.ok(createdBlog);
+//    }
+
 	@GetMapping
     public List<Blog> getAllBlogs() {
         return blogService.getAllBlogs();
@@ -40,7 +61,7 @@ public class AdminBlogController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // Thêm blog mới
+    //Thêm blog mới
     @PostMapping
     public Blog createBlog(@RequestBody Blog blog) {
         return blogService.createBlog(blog);
